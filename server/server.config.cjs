66c6fe4a390a72b6f085828cb09d26cb4471bc7f
tothg <toth.gergo88@hotmail.com>
2026-04-19 18:20:21 +0200

    Fix server.config.cjs

--- server/server.config.cjs
+++ server/server.config.cjs
@@ -1,18 +1,18 @@
 const path = require('path');
 
 module.exports = {
-    max_memory_restart: '2048M',
     apps: [
         {
             name: 'oxiclean',
             script: path.resolve(__dirname, '../dist/server.js'),
             instances: 'max',
-            exec_mode: 'cluster'
+            exec_mode: 'cluster',
+            // Az env blokknak az appon BELÜL kell lennie:
+            env: {
+                NODE_ENV: 'production',
+                PORT: 3001,
+                HOST: '127.0.0.1'
+            }
         }
-    ],
-    env: {
-        NODE_ENV: 'production',
-        PORT: 3001,
-        HOST: '127.0.0.1' // default
-    }
+    ]
 };
