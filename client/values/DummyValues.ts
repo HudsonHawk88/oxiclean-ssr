ea6c3623be25609d91d2233aff083034b2f6c52a
tothg <toth.gergo88@hotmail.com>
2026-04-26 12:25:54 +0200

    Add back referenciak route and upload pictures to referenciak

--- client/values/DummyValues.ts
+++ client/values/DummyValues.ts
@@ -49,27 +49,58 @@ export const services = [
     }
 ]
 
+const staticUrl = `https://oxiclean.hu/static`;
+const referenciakUrl = `${staticUrl}/referenciak`;
+
 export const referenciak = [
     {
         id: 1,
-        company: "OxiClean Ipari Park",
+        company: "Zalaegerszeg Aquapark",
         title: "Teljes tető- és homlokzattisztítás",
         images: [
-            { id: 101, url: "https://images.unsplash.com/photo-1628414443100-348630327f1f?auto=format&fit=crop&w=800&q=80", alt: "Tetőmosás munka közben" },
-            { id: 102, url: "https://images.unsplash.com/photo-1584622650111-993a426fbf0a?auto=format&fit=crop&w=800&q=80", alt: "Tiszta homlokzat" },
-            { id: 103, url: "https://images.unsplash.com/photo-1516880711640-ef7db81be3e1?auto=format&fit=crop&w=800&q=80", alt: "Ipari tisztítás" },
-            { id: 104, url: "https://images.unsplash.com/photo-1621905238294-4841d3941349?auto=format&fit=crop&w=800&q=80", alt: "Magasnyomású mosás" },
-            { id: 105, url: "https://images.unsplash.com/photo-1584622650111-993a426fbf0a?auto=format&fit=crop&w=800&q=80", alt: "Tiszta homlokzat" },
-            { id: 106, url: "https://images.unsplash.com/photo-1584622650111-993a426fbf0a?auto=format&fit=crop&w=800&q=80", alt: "Tiszta homlokzat" },
+            { id: 101, url: `${referenciakUrl}/1/78CED744-8374-4E1E-8226-F726F1EB9A41_4_5005_c.jpeg`, alt: "" },
+            { id: 102, url: `${referenciakUrl}/1/F6A90A50-D05E-43BE-94F9-0A0566C13582_4_5005_c.jpeg`, alt: "" },
         ]
     },
     {
         id: 2,
-        company: "Családi Ház - Zalaegerszeg",
+        company: "Privát",
         title: "Térkő tisztítás és impregnálás",
         images: [
-            { id: 201, url: "https://images.unsplash.com/photo-1589939705384-5185137a7f0f?auto=format&fit=crop&w=800&q=80", alt: "Térkő tisztítás előtt/után" },
-            { id: 202, url: "https://images.unsplash.com/photo-1632759162353-332f6a621b3b?auto=format&fit=crop&w=800&q=80", alt: "Ragyogó udvar" },
+            { id: 201, url: `${referenciakUrl}/2/2F1FE30E-8403-42F1-8177-7C4CD06474CF_4_5005_c.jpeg`, alt: "" },
+            { id: 202, url: `${referenciakUrl}/2/3CD772E0-17B7-400C-B581-8126E9DA386C_4_5005_c.jpeg`, alt: "" },
+            { id: 203, url: `${referenciakUrl}/2/9ABE859F-AF35-4F08-BAF7-8E1B18AFA600_4_5005_c.jpeg`, alt: "" },
+            { id: 204, url: `${referenciakUrl}/2/85FBDF3D-273B-4895-9E85-EB65B1C5F2CC_4_5005_c.jpeg`, alt: "" },
+            { id: 205, url: `${referenciakUrl}/2/7062AB7D-CA0D-44C0-BD21-E9F13A4F0A0F_4_5005_c.jpeg`, alt: "" },
+            { id: 206, url: `${referenciakUrl}/2/8045CFC7-C4E6-4C33-9A24-043FCD4E9F80_4_5005_c.jpeg`, alt: "" },
+            { id: 207, url: `${referenciakUrl}/2/44046AF0-5A25-405A-9A7B-FD22466CFEA9_4_5005_c.jpeg`, alt: "" },
+            { id: 206, url: `${referenciakUrl}/2/69054DFA-1F27-4BFA-A54B-AF4DFFC95F52_4_5005_c.jpeg`, alt: "" },
+            { id: 209, url: `${referenciakUrl}/2/A1F68D58-FCBD-48E0-9128-EF9C8456003A_4_5005_c.jpeg`, alt: "" },
+            { id: 210, url: `${referenciakUrl}/2/ADED5CAF-D5ED-40D6-838B-8FF264F656A5_4_5005_c.jpeg`, alt: "" },
+            { id: 211, url: `${referenciakUrl}/2/D85D0193-2BB7-4FE2-8B22-CA7E2027286A_4_5005_c.jpeg`, alt: "" },
+            { id: 212, url: `${referenciakUrl}/2/DC759340-2349-43EF-B35A-F8544EC1B0AA_4_5005_c.jpeg`, alt: "" },
+            { id: 213, url: `${referenciakUrl}/2/EE8A2CD7-5588-4E1E-AF79-718839DE95DE_4_5005_c.jpeg`, alt: "" },
+        ]
+    },
+    {
+        id: 3,
+        company: "Thermal Hotel Balance",
+        title: "Teljes tető- és homlokzattisztítás",
+        images: [
+            { id: 301, url: `${referenciakUrl}/3/0F3E3B85-7CA2-40A2-B6B8-6961D56C52F8_4_5005_c.jpeg`, alt: "" },
+            { id: 302, url: `${referenciakUrl}/3/93CDDB04-5695-4F6F-984D-8A567C6A3B17.JPG`, alt: "" },
+            { id: 303, url: `${referenciakUrl}/3/452CA110-5A49-42D4-921A-AECF299B57D4_4_5005_c.jpeg`, alt: "" },
+            { id: 304, url: `${referenciakUrl}/3/A36C7BF6-2164-4FAA-8EB5-6FDD8B272AB3.JPG`, alt: "" },
+            { id: 305, url: `${referenciakUrl}/3/EEF7F137-72AB-4E00-872A-3C1EC2EBC356_4_5005_c.jpeg`, alt: "" },
+        ]
+    },
+    {
+        id: 4,
+        company: "Zalaegerszegi Városi sport-, és tanuszoda",
+        title: "Ablakfelület és redőny tisztítás",
+        images: [
+            { id: 401, url: `${referenciakUrl}/4/D3E0BB48-2F1D-4994-9195-7F74EE0D7ADE_4_5005_c.jpeg`, alt: "" },
+            { id: 402, url: `${referenciakUrl}/4/F28497AC-250E-4F19-9D53-42D51A326E44_4_5005_c.jpeg`, alt: "" },
         ]
     }
 ]
